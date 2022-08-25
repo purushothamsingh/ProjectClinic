6fb0c8b6f6a11b61b3b2b65111afd2131493edbb
purushotham_singh <kpurushothamsingh@gmail.com>
2022-08-25 22:07:42 +0530

    Initial commit

---
+++ src/ClinicManagement/ClinicManagement/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace ClinicManagement
+{
+    internal class Program {
+        public static void Main(string[] args) {
+            Console.WriteLine("Project Clinic");
+        }
+    }
+
+
+
+}
+
