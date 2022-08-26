d3a9030498e0db7dc61e142d6c1e0ce86d5b7111
purushotham_singh <kpurushothamsingh@gmail.com>
2022-08-26 16:35:36 +0530

    removed ignored files

--- src/ClinicManagement/ClinicManagement/Program.cs
+++ src/ClinicManagement/ClinicManagement/Program.cs
@@ -12,11 +12,15 @@ namespace ClinicManagement
         public static ViewDoctors viewDoctors = new ViewDoctors();
         public static void Main()
         {
-            Console.WriteLine("Welcome to Clinic Management");
+            
             static bool  LoginCheck()
-            { 
+            {
+                Console.WriteLine("Welcome to Clinic Management");
                 Console.WriteLine();
+                Console.Write("Enter UserName ");
                 string username = Console.ReadLine();
+                Console.WriteLine();
+                Console.Write("Enter Password ");
                 string password = Console.ReadLine();
                 login = new Login(username, password);
                 if (login.ValidateCredentials())
@@ -31,7 +35,6 @@ namespace ClinicManagement
             static void ViewDoctorsInformation()
             {
                 dr2 = viewDoctors.ShowDoctorAvailability();
-
                 while (dr2.Read())
                 {
                     Console.WriteLine(dr2[0] + " "+ dr2[1] +" "+ dr2[2]+" " +dr2[3] + " " + dr2[4] + " " + dr2[5]+" " + dr2[6]);
@@ -39,11 +42,13 @@ namespace ClinicManagement
             }
             bool status = true;
             bool is_sucessful = false;
+
             while (status)
             {
                 bool result = LoginCheck();
                 if (result)
                 {
+                    Console.WriteLine();
                     Console.WriteLine("Login Sucessful");
                     status = false;
                     is_sucessful = true;
@@ -54,35 +59,43 @@ namespace ClinicManagement
                     Console.WriteLine("Invalid user name or password");
                 }
 
-            }
-
-            Console.WriteLine();
-            while (is_sucessful)
-            {
-                bool on_input = false;
                 Console.WriteLine();
-                Console.WriteLine("Press 1 to ViewDoctor Details");
-                Console.WriteLine("Press 2 to Add Patient Details");
-                Console.WriteLine("Press 3 to Schedule Appointment");
-                Console.WriteLine("Press 4 to Cancle Appointment");
-                Console.WriteLine("Press 5 to Logout");
-                Console.WriteLine("Please Select the option below to proceed");
-                int userinput = Convert.ToInt32(Console.ReadLine());
-                
 
-
-                switch (userinput)
+                while (is_sucessful)
                 {
-                    case 1:
-                        ViewDoctorsInformation();
-                        break;
-                    default:
-                        break;
+                    bool on_input = false;
+                    Console.WriteLine();
+                    Console.WriteLine("Press 1 to ViewDoctor Details");
+                    Console.WriteLine("Press 2 to Add Patient Details");
+                    Console.WriteLine("Press 3 to Schedule Appointment");
+                    Console.WriteLine("Press 4 to Cancle Appointment");
+                    Console.WriteLine("Press 5 to Logout");
+                    Console.WriteLine("Please Select the option below to proceed");
+                    int userinput = Convert.ToInt32(Console.ReadLine());
+                    switch (userinput)
+                    {
+                        case 1:
+                            ViewDoctorsInformation();
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                        case 4:
+                            break;
+                        case 5:
+                            is_sucessful = false;
+                            status = true;
+                            Console.WriteLine();
+                            Console.WriteLine("Logged out successfully");
+                            Console.WriteLine();
+                            break;
+
+                        default:
+                            break;
+                    }
+
                 }
-
-
-
-
             }
            
         }
